c15cd9c4a64213837c0e45dcea9be3d369b8084b
ThomasSpare <t.spare.jkpg@gmail.com>
2023-05-08 21:32:25 +0000

    fixed 99 problems in settings.py code suddenly got duplicated

--- SPOTIPY_SEARCH/settings.py
+++ SPOTIPY_SEARCH/settings.py
@@ -32,10 +32,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bangrad.herokuapp.com', 'localhost', ' *'
+ALLOWED_HOSTS = ['bangrad.herokuapp.com', 'localhost', ' *']
 # Application definition
 
-INSTALLED_APPS= [INSTALLED_APPS = [
+INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -48,7 +48,7 @@ INSTALLED_APPS= [INSTALLED_APPS = [
     'SPOTIPY_APP',
 ]
 
-MIDDLEWARE= [MIDDLEWARE = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,9 +58,9 @@ MIDDLEWARE= [MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF= 'SPOTIPY_SEARCH.urls'ROOT_URLCONF = 'SPOTIPY_SEARCH.urls'
+ROOT_URLCONF = 'SPOTIPY_SEARCH.urls'
 
-TEMPLATES= [TEMPLATES = [
+TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [TEMPLATES_DIR],
@@ -76,7 +76,7 @@ TEMPLATES= [TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION= 'SPOTIPY_SEARCH.wsgi.application'WSGI_APPLICATION = 'SPOTIPY_SEARCH.wsgi.application'
+WSGI_APPLICATION = 'SPOTIPY_SEARCH.wsgi.application'
 
 
 # Database
@@ -89,7 +89,7 @@ WSGI_APPLICATION= 'SPOTIPY_SEARCH.wsgi.application'WSGI_APPLICATION = 'SPOTIPY_S
 #    }
 # }
 
-DATABASES= {DATABASES = {
+DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
@@ -97,7 +97,7 @@ DATABASES= {DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS= [AUTH_PASSWORD_VALIDATORS = [
+AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
@@ -116,29 +116,28 @@ AUTH_PASSWORD_VALIDATORS= [AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE= 'en-us'LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE= 'UTC'TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC'
 
-USE_I18N= TrueUSE_I18N = True
+USE_I18N = True
 
-USE_L10N= TrueUSE_L10N = True
+USE_L10N = True
 
-USE_TZ= TrueUSE_TZ = True
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATICFILE_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = ospath.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = [os.path.join(BASE_DIR, 'staticfiles')]
 
-STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
-DEFAULT_FLE_STORAGE'
-DEFAULT_FILE_STORAGE= 'cloudinary_storage.storage.MediaCloudinaryStorage = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD= 'django.db.models.BigAutoField'DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
