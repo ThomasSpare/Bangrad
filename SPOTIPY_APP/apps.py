5782a3c05f1e833dfbbfb6cddddbaf2a3efcbe1f
Spare <t.spare.jkpg@gmail.com>
2023-05-09 11:53:57 +0000

    installed summernote

--- SPOTIPY_APP/apps.py
+++ SPOTIPY_APP/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class SpotipyAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'SPOTIPY_APP'
+
+
+class BlogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'blog'    
