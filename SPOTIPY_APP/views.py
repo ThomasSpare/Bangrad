0a4d17585ef1111b923ebc79d901c2038021ca41
Spare <t.spare.jkpg@gmail.com>
2023-05-09 17:57:39 +0000

    try add spotify search artist acoustic properties

--- SPOTIPY_APP/views.py
+++ SPOTIPY_APP/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views import generic, View
 from .models import Post
+from __future__ import print_function
+import json
+import spotipy
+import time
+import sys
 
 
+
+
+# shows acoustic features for tracks for the given artist
+
+client_credentials_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+sp.trace = True
+
+if len(sys.argv) > 1:
+    tids = sys.argv[1:]
+    print(tids)
+
+    start = time.time()
+    features = sp.audio_features(tids)
+    delta = time.time() - start
+    print(json.dumps(features, indent=4))
+    print("features retrieved in %.2f seconds" % (delta,))
+
+
+#  view post on blog (i think ? )
 
 class PostList(generic.ListView):
     model = Post
