8c72654392b12e61b631791fdc485d6e5fa3810e
ThomasSpare <t.spare.jkpg@gmail.com>
2023-05-10 11:23:58 +0000

    moved project to Code Anywhere

--- SPOTIPY_APP/views.py
+++ SPOTIPY_APP/views.py
@@ -1,32 +1,5 @@
 from django.shortcuts import render
-from django.views import generic, View
-from .models import Post
-from __future__ import print_function
-import json
-import spotipy
-import time
-import sys
-
-
 
 
 # shows acoustic features for tracks for the given artist
 
-if len(sys.argv) > 1:
-    tids = sys.argv[1:]
-    print(tids)
-
-    start = time.time()
-    features = sp.audio_features(tids)
-    delta = time.time() - start
-    print(json.dumps(features, indent=4))
-    print("features retrieved in %.2f seconds" % (delta,))
-
-
-#  view post on blog (i think ? )
-
-class PostList(generic.ListView):
-    model = Post
-    queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = "index.html"
-    paginate_by = 6
--- SPOTIPY_SEARCH/urls.py
+++ SPOTIPY_SEARCH/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("SPOTIPY_SEARCH.urls"), name="SPOTIPY_SEARCH-urls"),
+    path('summernote/', include('django_summernote.urls'))
 ]
