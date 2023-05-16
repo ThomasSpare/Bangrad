31e3290a805d0779e92116dfd2c965578bc3e1d7
ThomasSpare <t.spare.jkpg@gmail.com>
2023-05-16 18:36:07 +0000

    runtime.txt

--- SPOTIPY_APP/models.py
+++ SPOTIPY_APP/models.py
@@ -1,5 +1,18 @@
 from django.db import models
+from django import forms
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 import spotipy
 
+
+
+class BangradSearchFields(forms.Form):
+    Key = forms.CharField(max_length=2, required=False) 
+    Tempo = forms.IntegerField(max_length=3, required=False)
+    Language = models.CharField(max_length=20, required=False)
+    Release_year = forms.DateField(widget=forms.SelectDateWidget, required=False)
+ 
+    # renames the instances of the model
+    # with their title name
+    def __str__(self):
+        return self.title
--- SPOTIPY_APP/views.py
+++ SPOTIPY_APP/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.base import View
+from django.views.generic.edit import CreateView
+from .models import BangradSearchFields
+
+
+class BangradSearchFields(CreateView):
+ 
+    # specify the model for create view
+    model = BangradSearchFields
+    # specify the fields to be displayed
+    fields = ['Key', 'Tempo', 'Language', 'Release_year']
+
+
 
 
