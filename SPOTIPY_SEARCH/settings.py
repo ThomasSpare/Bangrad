3f453f6b994bb8a596f79932a9d189f6cc2bb280
ThomasSpare <t.spare.jkpg@gmail.com>
2023-05-31 16:13:07 +0000

    type error in allowed hosts

--- SPOTIPY_SEARCH/settings.py
+++ SPOTIPY_SEARCH/settings.py
@@ -44,7 +44,7 @@ DEBUG = False
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 
-ALLOWED_HOSTS = ["bangrad.herokuapp.com/", "localhost"]
+ALLOWED_HOSTS = ["bangrad.herokuapp.com", "localhost"]
 # Application definition
 
 INSTALLED_APPS = [
