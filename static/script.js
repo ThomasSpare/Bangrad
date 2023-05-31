86a6191318985f403805486607578f114ea210b8
ThomasSpare <t.spare.jkpg@gmail.com>
2023-05-31 21:40:57 +0000

    fixed spotipy redicet to heroku app

--- static/script.js
+++ static/script.js
@@ -31,7 +31,7 @@ $(document).ready(function () {
     let client_id = "343dc32e612a47cbbd658a30197599d1";
 
     let redirect_uri =
-        "https%3A%2F%2F8000-thomasspare-bangrad-5q314617lj.us2.codeanyapp.com";
+        "https%3A%2F%2Fbangrad.herokuapp.com";
     // *************** END *************************
 
     const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
