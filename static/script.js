6ce2e0407228f9d07fc2d2446917cee775638f87
Spare <t.spare.jkpg@gmail.com>
2023-08-09 05:52:40 +0000

    final commit from gitpod

--- static/script.js
+++ static/script.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
     // *************** REPLACE THESE VALUES! *************************
     let client_id = "343dc32e612a47cbbd658a30197599d1";
 
-    let redirect_uri = "https%3A%2F%2F8000-thomasspare-bangrad-sgwgrdlwu4y.ws-eu102.gitpod.io";
+    let redirect_uri = "https%3A%2F%2Fbangrad.herokuapp.com";
     
     // *************** END *************************
 
