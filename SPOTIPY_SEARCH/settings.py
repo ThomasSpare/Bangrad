31ed4e420f8fb58c2f5eabc5d2ac1322dfd40352
Spare <t.spare.jkpg@gmail.com>
2023-08-18 23:34:53 +0000

    Style responsive on small screen

--- SPOTIPY_SEARCH/settings.py
+++ SPOTIPY_SEARCH/settings.py
@@ -34,6 +34,7 @@ DEBUG = True
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ["bangrad.herokuapp.com",
+                "8000-thomasspare-bangrad-ovs3gah08at.ws-eu104.gitpod.io",
                 "*",
                 "8000-thomasspare-bangrad-1hovpn91ipy.ws-eu102.gitpod.io",
                 "8000-thomasspare-bangrad-b9cb8g84i0l.ws-eu102.gitpod.io",
