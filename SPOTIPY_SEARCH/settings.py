25e6a47a3ce63d5a69957faf41a7ff2d5e2d9267
Spare <t.spare.jkpg@gmail.com>
2024-01-01 16:13:05 +0000

    Static files settings

--- SPOTIPY_SEARCH/settings.py
+++ SPOTIPY_SEARCH/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'home')
 
 CKEDITOR_BASEPATH = "/staticfiles/ckeditor/ckeditor/"
 
